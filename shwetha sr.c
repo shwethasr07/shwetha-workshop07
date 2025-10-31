afeb6b96294e40377499aec8877b3af9b3ce0d65
Shwetha SR <shwethasr.07@gmail.com>
2025-10-31 16:15:45 +0530

    Add files via upload

---
+++ shwetha sr.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{ 
+	int a;
+	printf("enter any number");
+	scanf("%d",&a);
+	if (a>0)
+	printf("the given number is positive");
+	else
+	printf("the given number is negative");
+	
+}
+	
+
